4b7fccb3d4bdc5e4be158a5b2a061a2f8994dc5e
medvecky <alexey.medvecky@gmail.com>
2020-04-13 17:14:42 +0300

    Updates

---
+++ precache-manifest.e16fcf73e313d7e0189357b5a4f6d334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac917e806d1e580e5772b67f6ccc1f93",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "c353de2ba680233396e6",
+    "url": "/pomodoro-clock/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "35cec646eebeca491174",
+    "url": "/pomodoro-clock/static/css/main.8e7f9d2f.chunk.css"
+  },
+  {
+    "revision": "c353de2ba680233396e6",
+    "url": "/pomodoro-clock/static/js/2.6232ec89.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pomodoro-clock/static/js/2.6232ec89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cec646eebeca491174",
+    "url": "/pomodoro-clock/static/js/main.ed036909.chunk.js"
+  },
+  {
+    "revision": "c8e805114a65b007e849",
+    "url": "/pomodoro-clock/static/js/runtime-main.8a5682fa.js"
+  }
+]);
